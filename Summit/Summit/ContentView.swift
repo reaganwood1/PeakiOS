b9267f246454820f420d7123765a8be5f7c54094
Reagan Wood <reaganwood1@gmail.com>
2020-03-13 09:15:26 -0500

    initial working commit

---
+++ Summit/Summit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Summit
+//
+//  Created by Reagan Wood on 3/13/20.
+//  Copyright © 2020 Reagan Wood. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
