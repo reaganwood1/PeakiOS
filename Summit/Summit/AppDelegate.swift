bd86046367f408840e8d89b6ba85ebb19c26ecb8
Reagan Wood <reaganwood1@gmail.com>
2020-03-14 14:40:56 -0500

    add almofire

--- Summit/Summit/AppDelegate.swift
+++ Summit/Summit/AppDelegate.swift
@@ -10,7 +10,18 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        presentRootView()
+        return true
+    }
+    
+    private func presentRootView() {
+        let mainStoryboardIpad : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewControlleripad : UIViewController = mainStoryboardIpad.instantiateViewController(withIdentifier: "ViewController") as UIViewController
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = initialViewControlleripad
+        self.window?.makeKeyAndVisible()
     }
 }
