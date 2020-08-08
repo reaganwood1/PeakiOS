06b5d8e162139860076b4696ee8ab47e1629396d
Reagan Wood <reaganwood1@gmail.com>
2020-08-08 14:12:42 -0500

    Create extension for the ipad so that the popover doesn't crash on showing an alert.

--- Summit/Summit/Utilities/Extensions/UIViewController+Extensions.swift
+++ Summit/Summit/Utilities/Extensions/UIViewController+Extensions.swift
@@ -34,6 +34,12 @@ extension UIViewController {
             alertController.addAction(cancelAction)
         }
         
+        if let popoverController = alertController.popoverPresentationController {
+            popoverController.sourceView = self.view
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        
         self.present(alertController, animated: true, completion: nil)
     }
     
